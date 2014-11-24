a92cb3486106d8adcac931ddbe633856c1576140
Debs Noelie <ndebs@bs406-s16-11.insa-lyon.fr>
2014-11-24 18:22:04 +0100

    Check: add the right file

--- ppm.h
+++ ppm.h
@@ -8,16 +8,13 @@
 
 typedef struct mystruct
 {
-    int width; // Width
-    int height; // Heigth
-    u_char *image; //Tab blue green red
+  int width; // Width
+  int height; // Heigth 
+  u_char *image; //Tab blue green red
 }structure;
 
-
-
-
 //============================================================================
-//                           Function declarations
+// Function declarations
 //=========================width===================================================
 // Write the image contained in <data> (of size <width> * <height>)
 // into plain RGB ppm file, which name is <filename>
@@ -26,10 +23,9 @@ void ppm_write_to_file(int width, int height, u_char* data, char* filename);
 // into <data> and set <width> and <height> accordingly
 // Warning: data is malloc_ed, don't forget to free it
 void ppm_read_from_file(int *width, int *height, u_char** data);
-
 // Desaturate (transform to B&W) <image> (of size <width> * <height>)
 void ppm_desaturate(u_char* image, int width, int height);
-
 // Shrink image (of original size <width> * <height>) by factor <factor>
 // <width> and <height> are updated accordingly
 void ppm_shrink(u_char** image, int *width, int *height, int factor);
+
